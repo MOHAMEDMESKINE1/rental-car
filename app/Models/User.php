e40b0bb14f96d60bda03c475304ebcfc39522372
MOHAMEDMESKINE1 <meskinemohameddev@gmail.com>
2026-05-25 13:07:28 +0100

    fix display dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,77 @@
 
 namespace App\Models;
 
+// class User extends Authenticatable implements HasMedia
+// {
+//     use HasFactory, HasRoles, InteractsWithMedia, Notifiable;
+
+//     protected $fillable = [
+//         'name',
+//         'email',
+//         'password',
+//         'current_team_id',
+//     ];
+
+//     protected $hidden = [
+//         'password',
+//         'remember_token',
+//          'two_factor_secret',
+//         'two_factor_recovery_codes',
+//     ];
+
+//     protected function casts(): array
+//     {
+//         return [
+//             'email_verified_at' => 'datetime',
+//             'password'          => 'hashed',
+//             'two_factor_confirmed_at' => 'datetime',
+
+//         ];
+//     }
+
+
+use App\Concerns\HasTeams;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\Contracts\PasskeyUser;
+use Laravel\Fortify\PasskeyAuthenticatable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements HasMedia 
+
+#[Fillable(['name', 'email', 'password', 'current_team_id'])]
+#[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
+class User extends Authenticatable implements PasskeyUser, HasMedia
 {
-    use HasFactory, HasRoles, InteractsWithMedia, Notifiable;
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, HasTeams, Notifiable, InteractsWithMedia, HasRoles, PasskeyAuthenticatable, TwoFactorAuthenticatable {
+        // Resolve collision between HasTeams::teams and HasRoles::teams
+        HasTeams::teams insteadof HasRoles;
+        // If you still need the HasRoles teams method, alias it
+        HasRoles::teams as rolesTeams;
+    }
 
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'current_team_id',
-    ];
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-         'two_factor_secret',
-        'two_factor_recovery_codes',
-    ];
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
 
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password'          => 'hashed',
+            'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
-
         ];
     }
-
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('avatar')->singleFile();
